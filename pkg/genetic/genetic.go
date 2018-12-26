08a1cbaae910ad3584219df4fd5a845b67ca3543
Hyun-Bum Yang <hyunbumy@usc.edu>
2018-12-25 18:49:50 -0800

    Initial genetic algo implementation

---
+++ pkg/genetic/genetic.go
@@ -0,0 +1,30 @@
+package genetic
+
+import (
+	"math/rand"
+	"time"
+)
+
+// Run the genetic algorithm given the locations and the population size
+func Run(locations []Location, size int) [][]int {
+	randGen := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	return initialize(len(locations), size, randGen)
+}
+
+func initialize(locSize, size int, randGen *rand.Rand) [][]int {
+	population := make([][]int, size) // Make a list of possible routes
+	for i := 0; i < size; i++ {
+		population[i] = make([]int, locSize)
+		for j := 0; j < locSize; j++ {
+			population[i][j] = j
+		}
+
+		// Populate with random routes
+		tempSlice := population[1:]
+		randGen.Shuffle(len(tempSlice), func(i, j int) {
+			tempSlice[i], tempSlice[j] = tempSlice[j], tempSlice[i]
+		})
+	}
+	return population
+}
---
+++ pkg/genetic/location.go
@@ -0,0 +1,7 @@
+package genetic
+
+// Location struct to store location information
+type Location struct {
+	name                string
+	latitude, longitude float64
+}
